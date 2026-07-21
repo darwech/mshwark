74bd38da13226af85d2ce738143c73bdea493fe7
darwech <em34asd@gmail.com>
2026-07-21 08:00:13 +0300

    update splash screen animation

--- android/app/src/main/java/com/mshwark/app/MainActivity.java
+++ android/app/src/main/java/com/mshwark/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.mshwark.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // نحدد خلفية الـ WebView من أول لحظة تبقى نفس أزرق هوية التطبيق
+        // بدل اللون الأسود الافتراضي، عشان منشوفش أي ومضة سوداء بين
+        // شاشة الفتح الأصلية وشاشة الحركة
+        this.bridge.getWebView().setBackgroundColor(Color.parseColor("#0B2050"));
+    }
+}
